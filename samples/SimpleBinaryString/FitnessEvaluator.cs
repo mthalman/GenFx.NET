46b8ed44664a5c650a3e1448c1c922be146aba8a
Matt Thalman <mathalman@hotmail.com>
2017-01-02 13:44:01 -0700

    Add SimpleBinaryString sample project

---
+++ samples/SimpleBinaryString/FitnessEvaluator.cs
@@ -0,0 +1,32 @@
+﻿using GenFx;
+using GenFx.ComponentLibrary.Base;
+using GenFx.ComponentLibrary.Lists.BinaryStrings;
+using GenFx.Validation;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBinaryString
+{
+    [RequiredEntity(typeof(FixedLengthBinaryStringEntity))]
+    internal class FitnessEvaluator
+        : FitnessEvaluatorBase<FitnessEvaluator, FitnessEvaluatorConfiguration>
+    {
+        public FitnessEvaluator(IGeneticAlgorithm algorithm) : base(algorithm)
+        {
+        }
+
+        public override Task<double> EvaluateFitnessAsync(IGeneticEntity entity)
+        {
+            FixedLengthBinaryStringEntity binStrEntity = (FixedLengthBinaryStringEntity)entity;
+
+            // The entity's fitness is equal to the number of "true" bits (a bit representing a 1 value)
+            // it contains.
+            return Task.FromResult<double>(binStrEntity.Count(bit => bit == true));
+        }
+    }
+
+    internal class FitnessEvaluatorConfiguration
+        : FitnessEvaluatorConfigurationBase<FitnessEvaluatorConfiguration, FitnessEvaluator>
+    {
+    }
+}
