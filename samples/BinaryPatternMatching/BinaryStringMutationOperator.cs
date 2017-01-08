321fac02a581564eb6148b95f81528e6bc93b4fd
Matt Thalman <mathalman@hotmail.com>
2017-01-07 17:05:49 -0700

    Add BinaryPatternMatching sample project (#5)

---
+++ samples/BinaryPatternMatching/BinaryStringMutationOperator.cs
@@ -0,0 +1,44 @@
+﻿using GenFx;
+using GenFx.ComponentLibrary.Lists.BinaryStrings;
+using GenFx.Validation;
+
+namespace BinaryPatternMatching
+{
+    [RequiredEntity(typeof(VariableLengthBinaryStringEntity))]
+    public class BinaryStringMutationOperator : UniformBitMutationOperator<BinaryStringMutationOperator, BinaryStringMutationOperatorConfiguration>
+    {
+        public BinaryStringMutationOperator(IGeneticAlgorithm algorithm)
+            : base(algorithm)
+        {
+        }
+
+        protected override bool GenerateMutation(IGeneticEntity entity)
+        {
+            bool isMutated = false;
+
+            // In addition to the base mutation implementation, each bit has a
+            // probability (equal to the mutation rate) of being removed.
+            VariableLengthBinaryStringEntity binaryEntity = (VariableLengthBinaryStringEntity)entity;
+            for (int i = binaryEntity.Length - 1; i >= 0; i--)
+            {
+                if (RandomNumberService.Instance.GetRandomPercentRatio() <= this.Configuration.MutationRate)
+                {
+                    binaryEntity.RemoveAt(i);
+                    isMutated = true;
+                }
+            }
+
+            if (base.GenerateMutation(entity))
+            {
+                isMutated = true;
+            }
+
+            return isMutated;
+        }
+    }
+
+    public class BinaryStringMutationOperatorConfiguration
+        : UniformBitMutationOperatorConfiguration<BinaryStringMutationOperatorConfiguration, BinaryStringMutationOperator>
+    {
+    }
+}
---
+++ samples/BinaryPatternMatching/FitnessEvaluator.cs
@@ -0,0 +1,52 @@
+﻿using GenFx;
+using GenFx.ComponentLibrary.Base;
+using GenFx.ComponentLibrary.Lists.BinaryStrings;
+using GenFx.Validation;
+using System;
+using System.Threading.Tasks;
+
+namespace BinaryPatternMatching
+{
+    [RequiredEntity(typeof(VariableLengthBinaryStringEntity))]
+    internal class FitnessEvaluator
+        : FitnessEvaluatorBase<FitnessEvaluator, FitnessEvaluatorConfiguration>
+    {
+        public FitnessEvaluator(IGeneticAlgorithm algorithm) : base(algorithm)
+        {
+        }
+
+        public override Task<double> EvaluateFitnessAsync(IGeneticEntity entity)
+        {
+            VariableLengthBinaryStringEntity binaryEntity = (VariableLengthBinaryStringEntity)entity;
+
+            int totalBitDiffs = 0;
+            int minLength = Math.Min(binaryEntity.Length, this.Configuration.TargetBinary.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                bool bitValue = this.Configuration.TargetBinary[i] == '0' ? false : true;
+                if (binaryEntity[i] != bitValue)
+                {
+                    totalBitDiffs++;
+                }
+            }
+
+            // add the difference in size as part of the difference in bits
+            totalBitDiffs += Math.Abs(binaryEntity.Length - this.Configuration.TargetBinary.Length);
+
+            return Task.FromResult<double>(totalBitDiffs);
+        }
+    }
+
+    internal class FitnessEvaluatorConfiguration
+        : FitnessEvaluatorConfigurationBase<FitnessEvaluatorConfiguration, FitnessEvaluator>
+    {
+        private string targetBinary;
+
+        [CustomValidator(typeof(BinaryStringValidator))]
+        public string TargetBinary
+        {
+            get { return this.targetBinary; }
+            set { this.SetProperty(ref this.targetBinary, value); }
+        }
+    }
+}
