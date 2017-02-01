d2d5f36ba24e7edfde79e8f2314ee224a3543498
Matt Thalman <mathalman@hotmail.com>
2017-01-31 18:32:46 -0700

    Update versioning and product info (#20)

---
+++ src/version.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenFx.NET")]
+[assembly: AssemblyCopyright("Copyright © 2017 Matt Thalman")]
