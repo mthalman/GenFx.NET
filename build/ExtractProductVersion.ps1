ed8ca1bea6451fa2074dbcb8becbc055abe46bcb
Matt Thalman <mathalman@hotmail.com>
2017-02-03 11:48:40 -0700

    Update version build script to use assembly instead of code file to get version

--- build/ExtractProductVersion.ps1
+++ build/ExtractProductVersion.ps1
@@ -1,9 +1,5 @@
 param(
-	[string]$VersionFilePath
+	[string]$AssemblyFilePath
 )
 
-# Finds the version number from the product version file and sets it to an environment variable.
-
-$versionContent = Get-Content $VersionFilePath
-$result = $versionContent -match "AssemblyVersion\(""((\d+|\.)+)""\)"
-$env:GenFxVersion = $matches[1]
+$env:GenFxVersion = (Get-Item $AssemblyFilePath).VersionInfo.FileVersion
